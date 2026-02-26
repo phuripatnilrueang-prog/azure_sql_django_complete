d087ff47af9eeee1ebf66f1d35d268207cd1a36a
Phuripat Nilrueang <phuripatnilrueang@gmail.com>
2026-02-26 15:43:25 +0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 # API URL patterns
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),   
     path('stores/', views.StoreList.as_view(), name='store-list'),
     path('stores/<int:store_id>/', views.StoreDetailUpdateDelete.as_view(), name='store_detail'),
     path('stores/deleteAll/', views.StoreDeleteAll.as_view(), name='store_delete_all'),
@@ -13,9 +11,12 @@ urlpatterns = [
     path('products/deleteAll/', views.ProductDeleteAll.as_view(), name='product_delete_all'),
     path('users/', views.UserList.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('users/deleteAll/', views.UserDeleteAll.as_view(), name='user_delete_all'),
     path('orders/', views.OrderList.as_view(), name='order-list'),
     path('orders/<int:pk>/', views.OrderDetail.as_view(), name='order-detail'),
+    path('orders/deleteAll/', views.OrderDeleteAll.as_view(), name='order_delete_all'),
     path('reviews/', views.ReviewList.as_view(), name='review-list'),
+    path('reviews/deleteAll/', views.ReviewDeleteAll.as_view(), name='review_delete_all'),
     path('reviews/<str:pk>/', views.ReviewDetail.as_view(), name='review-detail'),
     path('connect-db/', views.check_db_connection, name='check-db'),
 ]
