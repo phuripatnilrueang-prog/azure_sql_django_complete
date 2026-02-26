1584754ec68092ac8a92bcd0a6b65ead3cd761a5
Phuripat Nilrueang <phuripatnilrueang@gmail.com>
2026-02-26 15:21:12 +0700

    Update urls.py

--- azure_project/urls.py
+++ azure_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.urls import path
+from . import views
 
 from api.views import api_root
 
@@ -12,4 +14,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('', api_root),
+    path('users/',        views.UserList.as_view(),     name='user-list'),
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
+    path('users/deleteAll/', views.UserDeleteAll.as_view(), name='user_delete_all'),
 ]
