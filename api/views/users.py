a20b58663e7cd9641eb32a16febde1aa3293103e
Phuripat Nilrueang <phuripatnilrueang@gmail.com>
2026-02-26 15:21:10 +0700

    Update users.py

--- api/views/users.py
+++ api/views/users.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from django.contrib.auth.models import User
 from ..serializers import UserSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserList(generics.ListCreateAPIView):
     # API view to list and create users
@@ -11,3 +14,8 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     # API view to retrieve, update or delete a user instance
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserDeleteAll(APIView):
+    def delete(self, request, *args, **kwargs):
+        User.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
